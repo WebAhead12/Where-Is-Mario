4e895d0e2da401f299dd1f5d42aec13773f64881
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2021-10-26 14:59:15 +0300

    more adding to github

--- githubInfo.js
+++ githubInfo.js
@@ -3,10 +3,17 @@ const input = document.querySelector("#username");
 const profileContainer = document.querySelector(".profileContainer");
 const output = document.querySelector(".output");
 
+input.addEventListener("click", () => {
+  input.value = "";
+});
 githubButton.addEventListener("click", () => {
   profileContainer.innerHTML = "";
-
   const usernameInput = input.value;
+  input.value = "";
+  if (usernameInput == "") {
+    profileContainer.innerHTML = "";
+    return;
+  }
   fetch(`https://api.github.com/users/${usernameInput}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
@@ -14,14 +21,16 @@ githubButton.addEventListener("click", () => {
     })
     .then((data) => {
       const profile = document.createElement("h2");
-      console.log(data.login);
+      const bio = document.createElement("div");
+      bio.textContent = data.bio;
       profile.textContent = data.login;
       const image = document.createElement("img");
       image.className = "profileImage";
       image.src = data.avatar_url;
-      image.alt = "";
+      image.alt = "profile pic";
       profileContainer.appendChild(image);
       profileContainer.appendChild(profile);
+      profileContainer.appendChild(bio);
       fetch(data.followers_url)
         .then((response) => {
           if (!response.ok) throw new Error(response.status);
@@ -32,13 +41,27 @@ githubButton.addEventListener("click", () => {
           followers.textContent = "followers: " + data.length;
           profileContainer.appendChild(followers);
         });
+      fetch(data.followings_url)
+        .then((response) => {
+          if (!response.ok) throw new Error(response.status);
+          return response.json();
+        })
+        .then((data) => {
+          const followings = document.createElement("div");
+          followings.textContent = "followings: " + data.length;
+          profileContainer.appendChild(followers);
+        });
     })
     .catch((error) => {
       console.log(error);
       if (error.message === "404") {
-        output.textContent = `⚠️ Couldn't find "${input.value}"`;
+        input.value = `⚠️ Couldn't find "${usernameInput}"`;
       } else {
-        output.textContent = "⚠️ Something went wrong";
+        input.value = "⚠️ Something went wrong";
       }
+      let tempVal = input.value;
+      setTimeout(() => {
+        if (input.value == tempVal) input.value = "";
+      }, 5000);
     });
 });
