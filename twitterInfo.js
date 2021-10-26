13aac6564c47f31aa546605f8f6d6dedfd866db7
Diabolica <scripter.Diabolica@gmail.com>
2021-10-26 13:14:58 +0300

    Weather Start

--- twitterInfo.js
+++
@@ -1,5 +0,0 @@
-let apiToken = "a8ykGO2CJeFtE5WIC9CSqpYqb";
-let secretToken = "xTa6ncj2Xl7v58EZGCa0ARnLRHUvpJMOtnbY2TNJziDup44mC2";
-let bearerToken = "AAAAAAAAAAAAAAAAAAAAAD6PVAEAAAAAZZ0WC4mnKJtJ3tST%2BUDLYRXKcfI%3DwICB9BMtPHIgXGgKvPrBwU7AEpLOknsdDhhWsU2CCYB57jhdIA";
-
-// fetch("URL", {headers: {`Bearer ${bearerToken}`}})
