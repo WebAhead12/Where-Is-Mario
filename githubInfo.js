349bd47f98dd4aed69d9a515cd893d0c9fc784e2
Diabolica <scripter.Diabolica@gmail.com>
2021-10-26 15:35:10 +0300

    ProfileImage

--- githubInfo.js
+++ githubInfo.js
@@ -3,7 +3,9 @@ const input = document.querySelector("#username");
 const profileContainer = document.querySelector(".profileContainer");
 const output = document.querySelector(".output");
 
-input.addEventListener("click", () => {
+const profileImage = document.querySelector(".profileImage");
+
+input.addEventListener("focusin", () => {
   input.value = "";
 });
 githubButton.addEventListener("click", () => {
@@ -24,11 +26,7 @@ githubButton.addEventListener("click", () => {
       const bio = document.createElement("div");
       bio.textContent = data.bio;
       profile.textContent = data.login;
-      const image = document.createElement("img");
-      image.className = "profileImage";
-      image.src = data.avatar_url;
-      image.alt = "profile pic";
-      profileContainer.appendChild(image);
+      profileImage.src = data.avatar_url;
       profileContainer.appendChild(profile);
       profileContainer.appendChild(bio);
       fetch(data.followers_url)
@@ -39,16 +37,6 @@ githubButton.addEventListener("click", () => {
         .then((data) => {
           const followers = document.createElement("div");
           followers.textContent = "followers: " + data.length;
-          profileContainer.appendChild(followers);
-        });
-      fetch(data.followings_url)
-        .then((response) => {
-          if (!response.ok) throw new Error(response.status);
-          return response.json();
-        })
-        .then((data) => {
-          const followings = document.createElement("div");
-          followings.textContent = "followings: " + data.length;
           profileContainer.appendChild(followers);
         });
     })
