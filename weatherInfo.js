dcfb58f7a3b78bd7958a0727778980656227b199
Diabolica <scripter.Diabolica@gmail.com>
2021-10-27 14:30:22 +0300

    Weather Changes

--- weatherInfo.js
+++ weatherInfo.js
@@ -3,7 +3,19 @@ let api_key = "21e90fff0e3a4567a31111147212610";
 let geoLon = "";
 let geoLat = "";
 
+let C = 0;
+let F = 0;
+
+const container = document.querySelector(".weatherContainer")
+const locationText = document.querySelector(".weatherLocation");
+const image = document.querySelector(".weatherImage");
+const temperatureText = document.querySelector(".weatherTemperature");
+const descriptionText = document.querySelector(".weatherDescription");
+
+const fahrenheitPreferance = window.localStorage.getItem("weatherPreferance") || false;
+
 if (navigator.geolocation) {
+
   navigator.geolocation.getCurrentPosition(function (pos) {
     geoLon = String(pos.coords.longitude.toFixed(5));
     geoLat = String(pos.coords.latitude.toFixed(5));
@@ -16,18 +28,24 @@ if (navigator.geolocation) {
         return response.json();
       })
       .then((weatherData) => {
-        const temperature = document.querySelector(".weatherTemperature");
-        temperature.innerHTML =
-          weatherData.current.temp_c + "<sup>&#176;</sup>";
-
-        const image = document.querySelector(".weatherImage");
+        C = weatherData.current.temp_c;
+        F = weatherData.current.temp_f;
+        if(fahrenheitPreferance)
+        temperatureText.innerHTML = F + "<sup>&#176;</sup>";
+        else
+        temperatureText.innerHTML = C + "<sup>&#176;</sup>";
         image.src = weatherData.current.condition.icon;
-        image.alt = "";
-        const location = document.querySelector(".weatherLocation");
-        location.textContent = weatherData.location.name;
-
-        const description = document.querySelector(".weatherDescription");
-        description.textContent = weatherData.current.condition.text;
+        locationText.textContent = weatherData.location.name;
+        descriptionText.textContent = weatherData.current.condition.text;
       });
   });
+  
+  container.addEventListener("click", (event)=>{
+    fahrenheitPreferance = !fahrenheitPreferance;
+    window.localStorage.setItem("weatherPreferance", fahrenheitPreferance);
+    if(fahrenheitPreferance)
+      temperatureText.innerHTML = F + "<sup>&#176;</sup>";
+    else
+      temperatureText.innerHTML = C + "<sup>&#176;</sup>";
+  })
 }
