fb401f724ad1fd0b294faea8012e37591c7c2c3d
Eitab <eitab.keis@gmail.com>
2021-10-27 09:37:04 +0300

    weather

--- weatherInfo.js
+++ weatherInfo.js
@@ -11,9 +11,29 @@ if (navigator.geolocation) {
     let url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${geoLat},${geoLon}&aqi=no`;
 
     fetch(url)
-      .then((resp) => resp.json())
-      .then((val) => {
-        console.log(val);
-      });
+    .then(response => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    .then(weatherData => {
+      console.log(weatherData);
+       const temperature=document.querySelector(".temperature");
+       temperature.innerHTML=weatherData.current.temp_c+ "<sup>&#176;</sup>"
+
+       const image = document.querySelector(".img");
+       image.src = weatherData.current.condition.icon;
+       image.alt = "";
+       const location=document.querySelector(".location");
+       location.textContent=weatherData.location.name;
+
+       const description=document.querySelector(".description");
+       description.textContent=weatherData.current.condition.text;
+
+
+    })
+
+    
+
   });
 }
+
