c40ba867e06628a9550181f1e78ac6a93ad9d01a
halakhamis07 <78309585+halakhamis07@users.noreply.github.com>
2021-10-28 11:21:07 +0300

    Js comments

--- githubInfo.js
+++ githubInfo.js
@@ -5,10 +5,11 @@ const infoContainer = document.querySelector(".infoContainer");
 const repoClone = document.querySelector(".cloneRepo").cloneNode(true);
 document.querySelector(".cloneRepo").remove();
 
+//when you click out of the search input and back in it clears
 inputText.addEventListener("focusin", () => {
   inputText.value = "";
 });
-
+//when you click enter it does the same affect as clicking the github button
 inputText.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
@@ -16,11 +17,11 @@ inputText.addEventListener("keyup", function (event) {
     githubButton.click();
   }
 });
+//the whole search of a github profile starts when the github button is clicked
 
 githubButton.addEventListener("click", () => {
   infoContainer.innerHTML = "";
   profileContainer.innerHTML = "";
-
   const usernameInput = inputText.value;
   inputText.value = "";
   if (usernameInput == "") {
@@ -32,33 +33,37 @@ githubButton.addEventListener("click", () => {
       if (!response.ok) throw new Error(response.status);
       return response.json();
     })
+    //creates the profile picture and the followings
     .then((data) => {
+      //name
       const profile = document.createElement("h2");
       profile.innerHTML = `<a href=${data.html_url}>${data.login}</a>`;
       profileContainer.appendChild(profile);
-
+      //bio
       const bio = document.createElement("div");
       bio.className = "bio";
       bio.innerHTML = `<strong>Bio</strong><div><p>${data.bio}</p></div>`;
       profileContainer.appendChild(bio);
-
+      //profile image
       const image = document.createElement("img");
       image.src = data.avatar_url;
       image.alt = "profile pic";
       image.className = "profileImage";
       profileContainer.appendChild(image);
-
+      //followings + followers
       const follow = document.createElement("div");
       follow.className = "follow";
       profileContainer.appendChild(follow);
-
+      //followers
       const followers = document.createElement("div");
       followers.textContent = "followers: " + data.following;
       follow.appendChild(followers);
-
+      //following
       const followings = document.createElement("div");
       followings.textContent = "following: " + data.followers;
       follow.appendChild(followings);
+
+      //repositories//
 
       fetch(data.repos_url)
         .then((response) => {
@@ -66,15 +71,16 @@ githubButton.addEventListener("click", () => {
           return response.json();
         })
         .then((dataRepo) => {
-          //repositories//
           const repoHeading = document.createElement("h1");
           repoHeading.innerHTML = "Repositories";
           repoHeading.style["text-decoration"] = "underline";
           repoHeading.style["font-family"] = `"Gelasio", serif`;
           infoContainer.appendChild(repoHeading);
+          //cloning the reposotories
           for (let repo of dataRepo) {
             let newRepo = repoClone.cloneNode("true");
-            let child = newRepo.children;
+            let child = newRepo.children; // to make line of code smaller (instead of typing
+            // newRepo.Child[] )//
             child[0].innerHTML = `<a href=${repo.html_url} target="_blank">${repo.name}</a>`; // hyper link for name of repo
             child[1].innerHTML = "Owner: ".bold() + repo.owner.login;
             child[2].innerHTML = "Description: ".bold() + repo.description;
